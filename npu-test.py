081014e0b61f335e424ee10626f6fdd3b54579cf
vv <vv@example.com>
2024-11-06 22:26:20 +0800

    add more test for manually computing matrix

--- npu-test.py
+++ npu-test.py
@@ -40,8 +40,10 @@ def do():
 total_diff = 0
 diff_count = 0
 def check():
-    a = torch.randn(1000, 1000, dtype=torch.float16)
-    b = torch.randn(1000, 1000, dtype=torch.float16)
+    # a = torch.randn(1000, 1000, dtype=torch.float16)
+    # b = torch.randn(1000, 1000, dtype=torch.float16)
+    a = torch.randn(100, 100, dtype=torch.float16)
+    b = torch.randn(100, 100, dtype=torch.float16)
 
     print("======================================")
     for i in range(1):
@@ -52,13 +54,84 @@ def check():
         if not c1.equal(c2):
             global total_diff, diff_count
             total_diff += 1
-            diff = c1.sub(c2).reshape((-1,))
-            nz = diff.nonzero()
+            diff = c1.sub(c2)
+            nz = diff.nonzero(as_tuple=True)
             diff_count += len(nz)
-            print(f"diff: idxs={nz}, values={diff[nz]}")
+            print(f"c1: {c1[nz].numpy()}")
+            print(f"c2: {c2[nz].numpy()}")
+            print(f"diff: values={diff[nz].numpy()}, idxs={nz}")
+            bb = b.transpose(0,1)
+            a1=a[nz[0]]
+            b1=bb[nz[1]]
+            result = []
+            for i in range(len(a1)):
+                sum = 0
+                for j in range(len(a1[0])):
+                    sum += a1[i][j].numpy() * b1[i][j].numpy()
+                result.append(sum)
+            print(f"manually result: {result}")
         else:
             print(f"Equal!")
 
-for i in range(10):
-    check()
-print(f"Total diff: {total_diff}, nz={diff_count}")
+# with torch.no_grad():
+#     for i in range(1):
+#         check()
+#     print(f"Total diff: {total_diff}, nz={diff_count}")
+import numpy
+with torch.no_grad():
+    a = torch.rand(100, 100, dtype=torch.float16)
+    b = torch.rand(100, 100, dtype=torch.float16)
+    cc = torch.matmul(a, b).numpy()
+    c1 = run_matmul(a, b.transpose(0,1)).numpy()
+    c2 = [[] for i in range(len(a))]
+    for i in range(len(a)):
+        for j in range(len(a)):
+            sum = 0.0
+            for k in range(len(a)):
+                sum += a[i][k] * b[k][j]
+            c2[i].append(sum.numpy())
+
+    diff_intel_manual = abs(c1 - c2)
+    diff_torch_manual = abs(cc - c2)
+    diff_torch_intel = abs(cc - c1)
+    print(f"diff_torch_intel: {len(diff_torch_intel.nonzero()[0])}, max: {diff_torch_intel.max()}, min: {diff_torch_intel.min()}")
+    print(f"diff_torch_manual: {len(diff_torch_manual.nonzero()[0])}, max: {diff_torch_manual.max()}, min: {diff_torch_manual.min()}")
+    print(f"diff_intel_manual: {len(diff_intel_manual.nonzero()[0])}, max: {diff_intel_manual.max()}, min: {diff_intel_manual.min()}")
+    # print(f"c1: {c1}")
+    # print(f"c2: {c2}")
+
+#
+# Copyright © 2024 Intel Corporation
+# SPDX-License-Identifier: Apache 2.0
+#
+
+# from intel_npu_acceleration_library.backend import MatMul
+# import numpy as np
+
+
+# def run_matmul(inC, outC, batch):
+
+#     # Create both inputs
+#     X1 = np.random.uniform(-1, 1, (batch, inC)).astype(np.float16)
+#     X2 = np.random.uniform(-1, 1, (outC, inC)).astype(np.float16)
+
+#     mm = MatMul(inC, outC, batch, profile=False)
+
+#     intel = mm.run(X1, X2)
+#     intel = torch.tensor(intel)
+
+#     tor = torch.matmul(torch.tensor(X1), torch.tensor(X2).transpose(-2, -1))
+#     print(tor.equal(intel))
+#     sub = tor.sub(intel)
+#     print(sub.shape)
+#     diff_idx = sub.nonzero()
+#     print(len(diff_idx))
+#     print(diff_idx)
+#     print(sub[sub.nonzero(as_tuple=True)])
+
+
+
+# if __name__ == "__main__":
+#     with torch.no_grad():
+#         result = run_matmul(128, 128, 32)
+#         print(result)
